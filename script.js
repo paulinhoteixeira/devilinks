ee2bc40a14edb88f639f95ceb672eee8f9f948f8
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2023-01-07 21:50:32 -0300

    add avatar swap in toggle function

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 function toggleMode() {
   const html = document.documentElement
-
-  // if(html.classList.contains('light')){
-  //   html.classList.remove('light')
-  // }else{
-  //   html.classList.add('light')
-  // }
-
   html.classList.toggle('light')
+
+  const img = document.querySelector("#profile img")
+
+  if(html.classList.contains('light')){
+    img.setAttribute('src', './assets/avatar-light.png')
+    img.setAttribute("alt", "Foto em preto e branco de Paulinho Teixeira")
+  }else{
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("alt", "Foto colorida de Paulinho Teixeira")
+  }
+
 }
